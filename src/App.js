e1bb642c434671a1f4792c6b5535cb8e3900555c
AsherCode <alonsosandovalch@gmail.com>
2022-08-23 18:38:23 -0500

    signin component added as well ass firebase utils and brough in firebase libraries

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import {Home} from './components/routes/home/home.component';
 import {Navigation} from './components/navigation/navigation.component';
 import { Routes, Route } from 'react-router-dom';
-
+import { SignIn } from './components/routes/signIn/signIn.component';
 export default function App () {
   return(
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
+        <Route path='sign-in' element={<SignIn />} />
       </Route>
     </Routes>
   )
