db2e270d4381323e9e32f92781e3cbded3717c41
AsherCode <alonsosandovalch@gmail.com>
2022-09-08 12:32:57 -0500

    added category reducer

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
-import { CategoriesProvider } from './contexts/categories.context';
 import { CartProvider } from './contexts/cart.context';
 
 import { Provider } from 'react-redux'; 
@@ -17,11 +16,9 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
         <BrowserRouter>
-            <CategoriesProvider>
-              <CartProvider>
-                <App />
-              </CartProvider>
-            </CategoriesProvider>
+            <CartProvider>
+              <App />
+            </CartProvider>
         </BrowserRouter>
     </Provider>
   </React.StrictMode>,
---
+++ src/store/categories/categories.types.js
@@ -0,0 +1,6 @@
+export const CATEGORIES_ACTION_TYPES = {
+    // we add the "category/" to be more explicit, avoid confusion and make it scalable
+  SET_CATEGORIES_MAP: 'categories/SET_CATEGORIES_MAP',
+};
+
+
